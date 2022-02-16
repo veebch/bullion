6327596ac5453def682f0dff442403612428a29c
veebch <mart@veeb.ch>
2022-02-16 22:16:54 +0100

    tweaks

--- bullion.py
+++ bullion.py
@@ -38,18 +38,37 @@ def makeSpark(pricestack):
     imgspk.close()
     return
 
-def updateDisplay(config, pricestack):
-    pass
+def updateDisplay(pricestack):
+    pricenow = pricestack[-1]
+    sparkbitmap = Image.open(os.path.join(picdir,'spark.bmp'))
+    pricechangeraw = round((pricestack[-1]-pricestack[0])/pricestack[-1]*100,2)
+    if pricechangeraw >= 10:
+        pricechange = str("%+d" % pricechangeraw)+"%"
+    else:
+        pricechange = str("%+.2f" % pricechangeraw)+"%"
+    if '24h' in config['display'] and config['display']['24h']:
+        timestamp= str(time.strftime("%-H:%M, %d %b %Y"))
+    else:
+        timestamp= str(time.strftime("%-I:%M %p, %d %b %Y"))
+    # This is where a locale change can be made
+    localetag = 'en_US' # This is a way of forcing the locale currency info eg 'de_DE' for German formatting
+    if pricenow > 10000:
+        # round to nearest whole unit of currency, this is an ugly hack for now
+        pricestring=custom_format_currency(int(pricenow), fiat.upper(), localetag)
+    image = Image.new('L', (264,176), 255)    # 255: clear the image with white
+    draw = ImageDraw.Draw(image)
+    draw.text((110,90),str(days_ago)+" day : "+pricechange,font =font_date,fill = 0)
+    writewrappedlines(image, pricestring,50,55,8,15,"Roboto-Medium" )
+    image.paste(sparkbitmap,(80,40))
+    draw.text((95,15),timestamp,font =font_date,fill = 0)
+#   Return the ticker image
+    return image
 
-def display_image():
+def display_image(img):
     epd = epd2in7.EPD()
     epd.Init_4Gray()
     epd.display_4Gray(epd.getbuffer_4Gray(img))
     epd.sleep()
-    thekeys=initkeys()
-#   Have to remove and add key events to make them work again
-    removekeyevent(thekeys)
-    addkeyevent(thekeys)
     return
 
 with open(configfile) as f:
@@ -79,6 +98,6 @@ for symbolnow in symbollist:
         pricestack.append(float(csvts[i]['close']))
     flipit=pricestack[::-1]
     makeSpark(flipit)
-    image=updateDisplay(config, flipit)
+    image=updateDisplay(flipit)
     display_image(image)
     time.sleep(refreshtime)
