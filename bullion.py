e80f4fbda1961c5c9dcf5a2892cd419daa2d954e
veebch <mart@veeb.ch>
2022-02-17 10:34:28 +0100

    formatting

--- bullion.py
+++ bullion.py
@@ -84,10 +84,10 @@ def updateDisplay(pricestack,fiat,symbolnow):
     draw = ImageDraw.Draw(image)
     draw.text((110,90),"1 day : "+pricechange,font =font_date,fill = 0)
     writewrappedlines(image, pricestring,50,50,8,15,"Roboto-Medium" )
-    image.paste(typeimage, (10,5))
+    image.paste(typeimage, (10,10))
     image.paste(sparkbitmap,(90,40))
     fontreduction=30-(len(symbolnow)-3)*5 # longer symbol, smaller font
-    _place_text(image,symbolnow,-80,5,fontreduction,"Roboto-Medium",0)
+    _place_text(image,symbolnow,-75,5,fontreduction,"Roboto-Medium",0)
     draw.text((95,15),timestamp,font =font_date,fill = 0)
 #   Return the ticker image
     return image
